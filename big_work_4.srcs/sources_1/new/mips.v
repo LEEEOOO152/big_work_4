20b8f16bed8423ebf880af076185373f0e09ed21
LEEEOOO <1048509754@qq.com>
2025-12-16 23:44:45 +0800

    12.16 AI检查了连线

--- big_work_4.srcs/sources_1/new/mips.v
+++ big_work_4.srcs/sources_1/new/mips.v
@@ -15,14 +15,9 @@ module mips(
     // NPC wires
     wire zero;
     wire [31:0] pc;
-    wire [15:0] offs_16;
-    wire [25:0] offs_26;
-    wire [31:0] reg_addr;
     wire [31:0] npc;
     // PC wires (all done)
     // GPR wires
-    wire [4:0] R_addr_1;
-    wire [4:0] R_addr_2;
     wire [4:0] W_addr;
     wire [31:0] W_data ;
     wire [31:0] R_data_1;
@@ -30,23 +25,19 @@ module mips(
     // ALU wires
     wire [31:0] data_1 = R_data_1;// from GPR
     wire [31:0] data_2 = R_data_2;// from GPR
-    wire [15:0] uint_16;
-    wire [15:0] int_16;
     wire [31:0] result;
     // DM wires
     wire [11:2] addr_dm = result[11:2];//#################
     wire [31:0] W_data_dm = R_data_2;
     wire [31:0] R_data_dm;
     // IM wires
-    wire [11:2] addr_im = pc[9:0];//###############
+    wire [11:2] addr_im = pc[11:2];
     // MUX_3_data wires
     wire [31:0] pc_plus_4 = pc + 4;
     wire [31:0] alu_result = result;
-    wire [31:0] data_from_DM;
-    wire [31:0] write_to_GPR = W_data;
+    wire [31:0] write_to_GPR;
+    assign W_data = write_to_GPR;
     // MUX_3_num wires
-    wire [4:0] rt;
-    wire [4:0] rd;
     
     // modules instantiation
     PC pc_inst(.clk(clk), .rst(rst), .npc(npc), .pc(pc));
@@ -57,6 +48,6 @@ module mips(
     ALU alu_inst(.alu_op(alu_op), .data_1(data_1), .data_2(data_2),.uint_16(full_instr[15:0]),.int_16(full_instr[15:0]) ,.s_b(s_b),.zero(zero), .result(result));
     DM dm_inst(.addr(addr_dm), .W_data(W_data_dm), .mem_write(mem_write), .R_data(R_data_dm));
     MUX_3_data mux_3_data_inst(.s_data_write(s_data_write), .pc_plus_4(pc_plus_4), .alu_result(alu_result), .data_from_DM(R_data_dm), .write_to_GPR(write_to_GPR));
-    MUX_3_num mux_3_num_inst(.s_num_write(s_num_write), .rt(full_instr[20:16]), .rd(full_instr[15:11]), .W_addr(W_addr));
+    MUX_3_num mux_3_num_inst(.s_num_write(s_num_write), .rt(full_instr[20:16]), .rd(full_instr[15:11]), .write_to_GPR(W_addr));
 
 endmodule
